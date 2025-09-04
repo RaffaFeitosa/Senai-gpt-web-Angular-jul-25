f3a60ae5bce4b56eae2b78dc796aaa7c51359050
RaffaFeitosa <rafael.feitosa91.silva@gmail.com>
2025-09-03 21:58:25 -0300

    Inicial ANGuLAR

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-screen.html',
   styleUrl: './login-screen.css'
 })
 export class LoginScreen {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    //Quando a tela iniciar.
+
+    //Inicia o formulario
+    //Cria o campo obrigatório de email.
+    //Cria o campo obrigatório de senha.
+    this.loginForm = this.fb.group({
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]]
+    });
+
+  }
+
+  onLoginClick() {
+
+    alert("Botão de login clicado.");
+
+    console.log("Email", this.loginForm.value.email);
+    console.log("Password", this.loginForm.value.password);
+
+  }
 
 }
