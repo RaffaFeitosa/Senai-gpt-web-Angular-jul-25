bffdea59215ae976df21a7210c2f021815b75b88
RaffaFeitosa <rafael.feitosa91.silva@gmail.com>
2025-09-04 21:58:38 -0300

    VALIDAÇÃO DE ACESSO

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -24,12 +24,48 @@ export class LoginScreen {
 
   }
 
-  onLoginClick() {
+  async onLoginClick() {
 
-    alert("Botão de login clicado.");
+    //alert("Botão de login clicado.");
 
     console.log("Email", this.loginForm.value.email);
     console.log("Password", this.loginForm.value.password);
+
+  //while (this.loginForm.value.email == "" && this.loginForm.value.password == "") {
+  //alert ("Preencha email e senha")  BOA TENTATIVA
+  if (this.loginForm.value.email == ""){
+    alert ("Preencha o e-mail")
+  return;
+  }
+
+  if (this.loginForm.value.password == ""){
+    alert ("Preencha a senha")
+  return;
+    }
+
+    }
+
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+
+
+
+      method: "POST", //ENVIAR,
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password,
+      })
+    });
+
+    console.log ("STATUS CODE" , response.status)
+
+    if(response.status>=200 && response.status <=299){
+      alert("Acesso Autorizado")
+    }else{
+      alert("Acesso Negado")
+    }
 
   }
 
